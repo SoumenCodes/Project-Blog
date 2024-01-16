d6562f054743763aad3b86a853c2eb550509612f
SoumenCodes <soumen19j@gmail.com>
2024-01-16 19:21:49 +0530

    first changes

---
+++ src/conf/conf.js
@@ -0,0 +1,7 @@
+const conf = {
+  appwriteUrl: String(process.env.React_AppWrite_URL),
+  appwriteProjectId: String(process.env.React_AppWrite_Project_ID),
+  appwriteCollectionId: String(process.env.React_AppWrite_Collection_ID),
+  appwriteBucketId: String(process.env.React_AppWrite_Bucket_ID),
+};
+export default conf;
