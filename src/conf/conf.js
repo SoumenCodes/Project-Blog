3b4398d105ae42bbf268f1d81452bd2506152587
SoumenCodes <soumen19j@gmail.com>
2024-02-13 10:52:50 +0530

    Fixes

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,8 +1,8 @@
 const conf = {
-  appwriteUrl: String(process.env.React_AppWrite_URL),
-  appwriteProjectId: String(process.env.React_AppWrite_Project_ID),
-  appwriteDatabaseId: String(process.env.React_AppWrite_Database_ID),
-  appwriteCollectionId: String(process.env.React_AppWrite_Collection_ID),
-  appwriteBucketId: String(process.env.React_AppWrite_Bucket_ID),
+  appwriteUrl: String(import.meta.env.VITE_AppWrite_URL),
+  appwriteProjectId: String(import.meta.env.VITE_AppWrite_Project_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_AppWrite_Database_ID),
+  appwriteCollectionId: String(import.meta.env.VITE_AppWrite_Collection_ID),
+  appwriteBucketId: String(import.meta.env.VITE_AppWrite_Bucket_ID),
 };
 export default conf;
